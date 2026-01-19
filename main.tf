f5ef7b125fc898c8871335d415222e6121405ebe
Nikita Kovalev <nick.tommybee37@gmail.com>
2026-01-19 12:33:01 +0100

    Uncomment to deploy all Distribution settings

--- main.tf
+++ main.tf
@@ -45,64 +45,64 @@ EOT
 }
 
 # 2. Defining the CloudFront Distribution
-# variable "supertab_connect_origin_id" {
-#   type    = string
-#   default = "supertab-connect-origin"
-# }
-#
-# variable "supertab_rewrite_license_path_function_arn" {
-#   type = string
-#   default = "<function_arn_from_aws_console>"
-# }
-#
-# resource "aws_cloudfront_distribution" "test_website" {
-#   enabled = true
-#
-#   origin {
-#     domain_name = "https://api-connect.sbx.supertab.co"
-#     origin_id   = var.supertab_connect_origin_id
-#
-#     custom_origin_config {
-#         http_port              = 80
-#         https_port             = 443
-#         origin_protocol_policy = "https-only"
-#         origin_ssl_protocols   = ["TLSv1.2"]
-#     }
-#   }
-#
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/license.xml"
-#     allowed_methods = ["GET", "HEAD"]
-#     cached_methods = ["GET", "HEAD"]
-#     target_origin_id = var.supertab_connect_origin_id
-#
-#     forwarded_values {
-#       query_string = false
-#       headers = ["Origin"]
-#
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-#
-#     function_association {
-#       event_type   = "viewer-request"
-#       function_arn = var.supertab_rewrite_license_path_function_arn
-#     }
-#
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-#
-#   restrictions {
-#     geo_restriction { restriction_type = "none" }
-#   }
-#
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+variable "supertab_connect_origin_id" {
+  type    = string
+  default = "supertab-connect-origin"
+}
+
+variable "supertab_rewrite_license_path_function_arn" {
+  type = string
+  default = "arn:aws:cloudfront::637423387169:function/supertab-rewrite-license-path"
+}
+
+resource "aws_cloudfront_distribution" "test_website" {
+  enabled = true
+
+  origin {
+    domain_name = "https://api-connect.sbx.supertab.co"
+    origin_id   = var.supertab_connect_origin_id
+
+    custom_origin_config {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "https-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/license.xml"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = var.supertab_connect_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = var.supertab_rewrite_license_path_function_arn
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
